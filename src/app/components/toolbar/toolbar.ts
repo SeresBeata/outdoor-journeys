ed946c63338e10932def63a690a58ad2f6f60a72
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-05 18:35:33 +0200

    Create toggle btn for light/dark mode

--- src/app/components/toolbar/toolbar.ts
+++ src/app/components/toolbar/toolbar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 @Component({
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatSlideToggleModule],
   selector: 'app-toolbar',
   styleUrl: './toolbar.css',
   templateUrl: './toolbar.html'
 })
-export class Toolbar {}
+export class Toolbar {
+  isDarkMode = false
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme')
+    this.isDarkMode = savedTheme === 'dark'
+    document.documentElement.classList.toggle('dark-mode', this.isDarkMode)
+  }
+
+  toggleDarkMode(enabled: boolean) {
+    this.isDarkMode = enabled
+    document.documentElement.classList.toggle('dark-mode', enabled)
+    localStorage.setItem('theme', enabled ? 'dark' : 'light')
+  }
+}
