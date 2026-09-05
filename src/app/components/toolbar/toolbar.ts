6134b052afe492a1e125a0e3af256b7b838af214
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-05 17:58:33 +0200

    Generate component toolbar

---
+++ src/app/components/toolbar/toolbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  imports: [],
+  selector: 'app-toolbar',
+  styleUrl: './toolbar.css',
+  templateUrl: './toolbar.html'
+})
+export class Toolbar {}
