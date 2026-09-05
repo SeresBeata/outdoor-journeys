2b2e42150a94cc60ffb057f7f3cfc1bd630d00f3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-05 19:51:15 +0200

    Update Home with hero section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core'
 import { Toolbar } from '../../components/toolbar/toolbar'
+import { Hero } from '../../components/sections/hero/hero'
 
 @Component({
-  imports: [Toolbar],
+  imports: [Toolbar, Hero],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html'
