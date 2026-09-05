6fb07b77c505c4c55558e82d9a2f65843e793006
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-05 17:56:45 +0200

    Add home to routes with redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { Home } from './pages/home/home'
 
-export const routes: Routes = [];
+// redirect to home
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Home
+  }
+]
