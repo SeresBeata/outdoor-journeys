6223662d17d1bd7019a1b9e62fe45eb2357ab6b0
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-17 11:53:07 +0200

    Add scroll handler to home.ts & update home.html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -8,4 +8,17 @@ import { Hero } from '../../components/sections/hero/hero'
   styleUrl: './home.css',
   templateUrl: './home.html'
 })
-export class Home {}
+export class Home {
+  onScroll(event: Event) {
+    const container = event.target as HTMLElement
+    const scrollY = container.scrollTop
+
+    const layers = container.querySelectorAll<HTMLElement>('.parallax')
+
+    layers.forEach((layer) => {
+      const speed = Number(layer.dataset['speed'] ?? 0)
+
+      layer.style.transform = `translateY(${scrollY * speed}px)`
+    })
+  }
+}
