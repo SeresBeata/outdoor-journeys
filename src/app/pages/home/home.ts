e5b381bb3a2e3b4dc10b58a43525da615038fb94
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-17 13:28:07 +0200

    Update scroll handler of home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -13,12 +13,57 @@ export class Home {
     const container = event.target as HTMLElement
     const scrollY = container.scrollTop
 
-    const layers = container.querySelectorAll<HTMLElement>('.parallax')
+    const hero = container.querySelector<HTMLElement>('.hero')
 
-    layers.forEach((layer) => {
-      const speed = Number(layer.dataset['speed'] ?? 0)
+    if (!hero) {
+      return
+    }
 
-      layer.style.transform = `translateY(${scrollY * speed}px)`
-    })
+    const heroHeight = hero.offsetHeight
+
+    // Progress of scrolling through the hero section
+    const progress = Math.min(scrollY / heroHeight, 1)
+
+    //layers of hero section
+    const sky = hero.querySelector<HTMLElement>('.sky')
+    const forest = hero.querySelector<HTMLElement>('.forest')
+    const mountain = hero.querySelector<HTMLElement>('.mountain')
+    const fog = hero.querySelector<HTMLElement>('.fog')
+    const title = hero.querySelector<HTMLElement>('h1')
+
+    if (sky) {
+      sky.style.transform = `
+      translateY(${scrollY * 0.1}px)
+      scale(${1 + progress * 0.05})
+    `
+    }
+
+    if (forest) {
+      forest.style.transform = `
+      translateY(${scrollY * 0.3}px)
+      scale(${1 + progress * 0.08})
+    `
+    }
+
+    if (mountain) {
+      mountain.style.transform = `
+      translateY(${scrollY * 0.3}px)
+      scale(${1 + progress * 0.08})
+    `
+    }
+
+    if (fog) {
+      fog.style.transform = `
+      scale(${1 + progress * 0.12})
+    `
+    }
+
+    if (title) {
+      title.style.transform = `
+      translateY(${scrollY * 0.2}px)
+      scale(${1 - progress * 0.15})
+    `
+      title.style.opacity = `${1 - progress * 1.2}`
+    }
   }
 }
