2422682255acc290880f6c38c85f1ad559902550
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-22 16:00:20 +0200

    Create component WeatherAnimation

---
+++ src/app/components/weather-animation/weather-animation.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { WeatherAnimation } from './weather-animation'
+
+describe('WeatherAnimation', () => {
+  let component: WeatherAnimation
+  let fixture: ComponentFixture<WeatherAnimation>
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WeatherAnimation]
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(WeatherAnimation)
+    component = fixture.componentInstance
+    await fixture.whenStable()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ src/app/components/weather-animation/weather-animation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  imports: [],
+  selector: 'app-weather-animation',
+  styleUrl: './weather-animation.css',
+  templateUrl: './weather-animation.html'
+})
+export class WeatherAnimation {}
