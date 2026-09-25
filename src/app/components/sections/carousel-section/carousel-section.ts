4cacfac6fbbb5dfe8dc55c5975af6df24136d424
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-25 09:26:15 +0200

    Create carousel-section

---
+++ src/app/components/sections/carousel-section/carousel-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  imports: [],
+  selector: 'app-carousel-section',
+  styleUrl: './carousel-section.css',
+  templateUrl: './carousel-section.html'
+})
+export class CarouselSection {}
