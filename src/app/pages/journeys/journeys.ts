2f56958baa9674754f8d452cab897f342de3c6af
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-26 12:57:27 +0200

    Create pages/journeys

---
+++ src/app/pages/journeys/journeys.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  imports: [],
+  selector: 'app-journeys',
+  styleUrl: './journeys.css',
+  templateUrl: './journeys.html'
+})
+export class Journeys {}
