05d36200726d92fa823ae4e2107c99c6f50e68a7
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2026-09-26 12:48:15 +0200

    Update Carousel component

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core'
+import { NgbCarousel, NgbSlide } from '@ng-bootstrap/ng-bootstrap/carousel'
+import { environment } from '../../../environments/environment'
 
 @Component({
-  imports: [],
+  imports: [NgbCarousel, NgbSlide],
   selector: 'app-carousel',
   styleUrl: './carousel.css',
   templateUrl: './carousel.html'
 })
-export class Carousel {}
+export class Carousel {
+  images = [16, 29, 62, 66, 83, 984, 125, 128].map((n) => {
+    return {
+      src: `${environment.carouselUrl}${n}/900/500`,
+      link: 'link'
+    }
+  })
+}
